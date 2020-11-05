ba122ad12d78ad8eb0278dc32ecdf0110686ebd5
AshishSah10 <41590024+AshishSah10@users.noreply.github.com>
2020-11-05 19:25:44 +0530

    change1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
+  const dreamProjects = [
+    {
+      name: "ToDoList App",
+      description: "It will contain things we schedule for a day."
+    },
+    {
+      name: "IMDB Movie App",
+      description: "API based web app for searching about movies."
+    }
+  ];
   return (
-    <></>
-  )
-}
-
+    <ol>
+      {dreamProjects.map((project, index) => (
+        <li key={`${index}`}>
+          <p data-ns-test="project-name">{project.name}</p>
+          <p data-ns-test="project-description">{project.description}</p>
+        </li>
+      ))}
+    </ol>
+  );
+};
 
 export default App;
